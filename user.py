b1929bf6b6e550e20be0bc7d66b016855c1768fc
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-20 17:39:02 +0300

    added a statement to check password

--- user.py
+++ user.py
@@ -8,9 +8,9 @@ class User:
     This class' purpose is to create instances of User
     """
 
-    #user_list = [] #this creates an empty class of users
+    user_list = [] #this creates an empty class of users
 
-    def __init__(self,user_name:string, email:string ,password:string):
+    def __init__(self,user_name, email ,password):
         """
         this method will define User class properties
 
@@ -24,3 +24,27 @@ class User:
 
     def save_user(self):
         User.user_list.append(self)
+
+    @classmethod
+    def find_user_by_email(cls,email):
+        for user in cls.user_list:
+            if user.email == email:
+                return user
+        return False
+
+    @classmethod
+    def user_exists(cls, email,password):
+        for user in cls.user_list:
+            if user.email == email and user.password == password:
+                return True
+
+        return False
+    
+    # @classmethod
+    # def password_exists(cls, password):
+    #     for password in cls.user_list:
+    #         if password.password == password:
+    #             return True
+        
+    #     return False
+    
