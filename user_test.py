93ea5ccc992416bb88cfdc34d1beac79daa85999
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-20 09:19:13 +0300

    successfully tested object initialization

--- user_test.py
+++ user_test.py
@@ -2,11 +2,28 @@ import unittest
 import pyperclip
 
 from user import User
-#import required modules
+# import required modules
 
-class TestUser(unittest.TestCase): #import Testcase module to show that this is a test class
 
-    def setUp(self): #initialize appropriate values before running tests
-        self.new_user = User("First","secret word","example@mail.com")
+class TestUser(unittest.TestCase):  # import Testcase module to show that this is a test class
 
+    def setUp(self):  # initialize appropriate values before running tests
+        self.new_user = User("Brayo", "example@mail.com", "abc123",)
+
+    def tearDown(self):  # method called to initialize everything back to base after each test case
+        User.user_list = []
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"Brayo")
+        self.assertEqual(self.new_user.email,"example@mail.com")
+        self.assertEqual(self.new_user.password,"abc123")
     
+    def test_save_user(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
