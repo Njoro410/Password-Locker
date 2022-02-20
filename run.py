9d8219e9c2d959a3f57f79c8ee102008153f8dd0
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-20 11:54:01 +0300

    created new user and credential instances in

--- run.py
+++ run.py
@@ -2,4 +2,17 @@
 
 # change the mode (chmod +x run.py) in terminal
 
+from user import User
+from credentials import Credential
+
+#create a new user
+def create_user(name,email,password):
+    new_user = User(name,email,password)
+    return new_user
+
+#create new credential
+def create_credential(account,uname,pword,mail):
+    new_credential = Credential(account,uname,pword,mail)
+    return new_credential
+
 
