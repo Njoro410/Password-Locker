d3579e0f56d808dfefe0329094f8fcc2265f6148
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-20 11:20:57 +0300

    added a method tho test saving a user

--- user_test.py
+++ user_test.py
@@ -22,8 +22,8 @@ class TestUser(unittest.TestCase):  # import Testcase module to show that this i
         self.assertEqual(self.new_user.password,"abc123")
     
     def test_save_user(self):
-
-
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
 
 if __name__ == '__main__':
     unittest.main()
