506f1ac626e0b61d45c9047192570f9b87704395
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-20 10:33:20 +0300

    added save credential and delete credential methods

---
+++ credentials.py
@@ -0,0 +1,28 @@
+import pyperclip
+
+class Credential:
+    """
+    this class will create instances of credentials
+    """
+
+    credential_list = [] #creates an empty list of credentials
+
+    def __init__(self,account_name,user_details,secret_key,email):
+        """
+        this method will define credential class properties
+
+        :param account_name:
+        :param user_details:
+        :param password:
+        :param email:
+        """
+        self.account_name = account_name
+        self.user_details = user_details
+        self.secret_key = secret_key
+        self.email = email
+
+    def save_credential(self):
+        Credential.credential_list.append(self)
+    
+    def delete_credential(self):
+        Credential.credential_list.remove(self)
